4e99c0560df0d3f6a82357fc0c92a1d687557657
songpeng <419626398@qq.com>
2025-04-16 09:35:01 +0800

    docs:add document

--- README.md
+++ README.md
@@ -41,7 +41,8 @@ Norm Axios 是一个基于 Axios 的约定式请求库，提供了约定式的�
 
 ## 文档
 
-[详细文档](https://songpeng154.github.io/norm-axios/)
+[地址1(Cloudflare)](https://norm-axios-press.pages.dev/document/use-request/basic-usage)
+[地址2(Github)](https://songpeng154.github.io/norm-axios-press/)
 
 ## 致谢
 
--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -2,14 +2,14 @@ import definePlugin from './request/define-plugin.ts'
 import { clearCache, getCacheAll } from './request/utils/cache.ts'
 
 export * from './debounce/index'
+
 export * from './global/index'
-
 export * from './global/types'
+
 export * from './pagination/index'
-
 export * from './pagination/types'
-export * from './request/index'
 
+export * from './request/index'
 export * from './request/types'
 export * from './throttle/index'
 
